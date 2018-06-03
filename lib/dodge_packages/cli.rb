598d89ff0387e2e6b3fa4b833820c6f5d08444b7
Daniel-Mack <daniel.mack@powerhrg.com>
2018-06-03 03:04:02 -0400

    added more object functionality to the cli file.

--- lib/dodge_packages/cli.rb
+++ lib/dodge_packages/cli.rb
@@ -8,66 +8,27 @@ class DodgePackages::CLI
   end
 
   def listing # lists the vehicles
-    puts "Select a car to view packages for or type exit to close the program."
-    @packages = DodgePackages::Package.today
-  end
-
-
-  def challenger_packages #helper method
-    input = nil
-    while input != "exit"
-    input = gets.strip.downcase
-      case input
-      when "1"
-        puts "Blacktop info"
-        puts "Enter another number to see a different package or type exit to quit."
-      when "2"
-        puts "R/T Classic info"
-        puts "Enter another number to see a different package or type exit to quit."
-      when "3"
-        puts "Scat Pack"
-        puts "Enter another number to see a different package or type exit to quit."
-      when "exit"
-        exit
-      else
-        puts "Please enter a valid option."
-      end
-    end
-  end
-
-  def charger_packages #helper method
-    input = nil
-    while input != "exit"
-    input = gets.strip.downcase
-      case input
-      when "1"
-        puts "Blacktop info"
-        puts "Enter another number to see a different package or type exit to quit."
-      when "2"
-        puts "Charger Plus info"
-        puts "Enter another number to see a different package or type exit to quit."
-      when "3"
-        puts "Charger Premium info"
-        puts "Enter another number to see a different package or type exit to quit."
-      when "exit"
-        exit
-      else
-        puts "Please enter a valid option."
-      end
+    @packages = DodgePackages::Package.all
+    @packages.each.with_index(1) do |package, i|
+      puts "#{i}. #{package.name}"
     end
   end
 
   def selection #makes a package selection
      input = nil
      while input != "exit"
+     puts "Enter a number corresponding to a package to view it's features, type list to return to the menu or type exit to close the program."
       input = gets.strip.downcase
-      case input
-      when "1"
-        puts "Please select a package for the Dodge Challenger to view.  1. Blacktop, 2. R/T Classic, 3. Scat Pack "
-        challenger_packages
-      when "2"
-        puts "Please select a package for the Dodge Charger to view.  1. Blacktop, 2. Plus Group, 3. Premium Group"
-        charger_packages
+
+      if input.to_i > 0
+         a_package = @packages[input.to_i-1]
+         puts "#{a_package.name} - #{a_package.features}"
+      elsif input == "list"
+         listing
+      elsif input == "exit"
+        exit
+      else
+        puts "Please enter a valid option."
       end
     end
   end
--- lib/dodge_packages/package.rb
+++ lib/dodge_packages/package.rb
@@ -1,47 +1,36 @@
-require 'pry'
 class DodgePackages::Package
-  attr_accessor :name, :interior, :exterior, :url
+  attr_accessor :name, :features, :url
 
-  def self.today
-    # puts <<-DOC.gsub!(" ", "")
-    # 1. Challenger
-    # 2. Charger
-    # DOC
-
+  def self.all
     package_1 = self.new
     package_1.name = "DODGE CHALLENGER BLACKTOP PACKAGE"
-    package_1.interior = "features"
-    package_1.exterior  = "features"
+    package_1.features = "features"
     package_1.url = "https://www.dodge.com/challenger/packages.html"
 
     package_2 = self.new
     package_2.name = "DODGE CHALLENGER R/T CLASSIC PACKAGE"
-    package_2.interior = "features"
-    package_2.exterior  = "features"
+    package_2.features = "features"
     package_2.url = "https://www.dodge.com/challenger/packages.html"
 
     package_3 = self.new
     package_3.name = "DODGE CHALLENGER SCAT PACK APPEARANCE GROUP"
-    package_3.interior = "features"
-    package_3.exterior  = "features"
+    package_3.features = "features"
     package_3.url = "https://www.dodge.com/challenger/packages.html"
 
-     package_4 = self.new
+    package_4 = self.new
     package_4.name = "DODGE CHARGER BLACKTOP® PACKAGE"
-    package_4.interior = "features"
-    package_4.exterior  = "features"
+    package_4.features = "features"
     package_4.url = "https://www.dodge.com/challenger/packages.html"
 
-     package_5 = self.new
+    package_5 = self.new
     package_5.name = "DODGE CHARGER PLUS GROUP"
-    package_5.interior = "features"
-    package_5.exterior  = "features"
+    package_5.features = "features"
     package_5.url = "https://www.dodge.com/challenger/packages.html"
 
-     package_6 = self.new
+    package_6 = self.new
     package_6.name = "DODGE CHARGER PREMIUM GROUP"
-    package_6.interior = "features"
-    package_6.exterior  = "features"
+    package_6.features = "features"
     package_6.url = "https://www.dodge.com/challenger/packages.html"
+    [package_1, package_2, package_3, package_4, package_5, package_6]
   end
 end
