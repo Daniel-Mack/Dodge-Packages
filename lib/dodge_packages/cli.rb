bfe1fdaa69d25e3a52f4234f6f41e16e8af3aec3
Daniel-Mack <daniel.mack@powerhrg.com>
2018-06-07 16:09:14 -0400

    added the ability to choose between two vehicles and refactored charger scraper methods

--- lib/dodge_packages/cli.rb
+++ lib/dodge_packages/cli.rb
@@ -1,29 +1,56 @@
 class DodgePackages::CLI
 
   def call #runs the program
+    clear_page
     puts "Welcome to the Dodge Packages Program!"
-    listing
+    pick_car
     selection
     end_program
   end
 
-  def listing # lists the vehicles
-    @packages = DodgePackages::Package.available
+  def challenger_listing # lists the packages
+    @packages = DodgePackages::Package.challenger_available
     @packages.each.with_index(1) do |package, i|
       puts "#{i}. #{package.name}"
     end
   end
 
+  def charger_listing # lists the packages
+    @packages = DodgePackages::Package.charger_available
+    @packages.each.with_index(1) do |package, i|
+      puts "#{i}. #{package.name}"
+    end
+  end
+
+  def pick_car
+    puts "Which Dodge vehicle do you want to see packages for?"
+    puts <<-DOC.gsub(" ", "")
+    1. Challenger
+    2. Charger
+    DOC
+    input = gets.chomp
+    if input == "1"
+      puts "loading.."
+      split_line
+      challenger_listing
+    elsif input == "2"
+      puts "loading.."
+      split_line
+      charger_listing
+    else
+    end
+  end
+
   def selection #makes a package selection
+
     input = nil
     while input != "exit"
-      puts "Enter a number corresponding to a package to view it's features, type list to return to the menu or type exit to close the program."
+      puts "-------------------------------------------"
+      puts "Enter a number corresponding to a package to view it's features or type exit to close the program."
       input = gets.strip.downcase
       if input.to_i > 0
         a_package = @packages[input.to_i-1]
         puts "#{a_package.features}"
-      elsif input == "list"
-        listing
       elsif input == "exit"
         end_program
         exit
@@ -33,7 +60,16 @@ class DodgePackages::CLI
     end
   end
 
+  def clear_page
+    system "clear"
+  end
+
+  def split_line
+    puts "-------------------------------------------"
+    puts "-------------------------------------------"
+  end
+
   def end_program #ends program
-    puts "Program terminating.."
+    puts "Thanks for considering Dodge!"
   end
 end
